9ecc251daf9d01ba996e73b1f7cf1657a46291fe
natanaelparhusip <118708273+natanaelparhusip@users.noreply.github.com>
2022-11-21 12:22:03 +0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/project_3_pertemuan_6_textwidget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project_3_pertemuan_6_textwidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\flutter_windows_3.3.4-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\KULIAH\SEMESTER 5\Student Exchange (Pemrograman android)\Tugas\flutter application\project_3_pertemuan_6_textwidget"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
